609f530e1ebbb747fe04cb43ff495fb0def6d0fb
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-05-30 22:48:22 +0600

    js basic assainemt

---
+++ [name].js
@@ -0,0 +1,166 @@
+
+
+
+
+
+// 1st question start
+let carName = "Volvo";
+console.log(carName);
+// 1st question end
+
+
+// 2nd question start
+let firstName = "John";
+let lastName = "Doe";
+let age = 35 ;
+
+console.log(firstName+ " " + lastName + " " + age);
+// 2nd question end
+
+
+
+
+
+// 3rd question start
+
+let x = 10;
+let y = 5;
+x  = x * y;
+
+console.log(x);
+// 3rd question end
+
+
+
+
+
+// 4th question start
+
+let length = 16;
+console.log(typeof length); 
+// number
+
+let LastName = "johnson"
+console.log(typeof LastName);
+// string
+
+
+ x = {
+    firstName:"john",
+    lastName:"Doe"
+}
+console.log(typeof x);
+
+// 4th question end
+
+
+
+// 5th question start
+
+function myFunction(){
+    alert("Hello World!");
+}
+
+myFunction()
+
+// 5th question end
+
+
+
+
+// 6th question start
+
+const person = {
+    name: "John", 
+    age:50
+}
+alert(`"${person.name} is ${person.age}"`);
+
+
+// 6th question end
+
+
+
+
+// 7th question start
+
+           // button created in html file with alert
+
+// 7th question end
+
+
+
+// 8th question start
+
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert("the number of items is "+ cars.length);
+alert("frist item is " +cars[0])
+alert("second item is " +cars[1])
+alert("third item is " +cars[2])
+
+
+
+
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+
+     Brand[0]="Ford";
+
+   console.log(Brand);
+
+
+
+// 8th question end
+
+
+
+
+// 9th question start
+
+const randomNumber = Math.random();
+console.log("random number is "+ randomNumber);
+
+
+
+
+const largestNumber  = Math.max(10,11,12,13,14,15,16,17,18,19,20)
+console.log("The largest number from 10 - 20 is "+largestNumber);
+
+
+
+let sqrNumber = Math.sqrt(9);
+console.log("qure root number is " + sqrNumber);
+
+
+
+// 9th question end
+
+
+
+
+
+
+// 10th question start
+
+
+x = 10;
+y = 5;
+
+if (x>y) {
+    alert("x is greater than  y")   
+}
+
+
+
+
+
+
+age = 18;
+age<20?alert("Too young"):alert ("Old enough")
+
+
+// 10th question end
+
+
+
